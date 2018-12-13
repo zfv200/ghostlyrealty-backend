5b8c9807f72f599cf5c4b0ea23b89a4f2be66e69
zfv200 <zach.vary@flatironschool.com>
2018-12-13 17:58:40 -0500

    custom action for featured houses

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,4 +1,10 @@
 class House < ApplicationRecord
   has_many :haunts
   has_many :ghosts, through: :haunts
+
+  def self.featured
+    House.select do |house|
+      house.featured==true
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_213550) do
+ActiveRecord::Schema.define(version: 2018_12_13_211950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_213550) do
     t.datetime "updated_at", null: false
     t.string "image_url"
     t.string "name"
+    t.boolean "featured"
   end
 
 end
