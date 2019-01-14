f6286185a9c86ca5c75b9b9c17a4c3bae06098f3
zfv200 <zach.vary@flatironschool.com>
2019-01-14 16:23:34 -0500

    incorporated houses into results

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::SearchesController < ApplicationController
+  # respond_to :json
 
   def search_site
     query = params[:search][:searchTerm]
     search = Search.new
-    render :json => search.search_agents(query)
+    agents = search.search_agents(query)
+    houses = search.search_houses(query)
+    results = {:agents=>agents, :houses=>houses}
+    render :json => {:agents=>agents, :houses=>houses}.to_json
   end
 
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,9 +1,14 @@
 class Search
 
   def search_agents(query)
-    agents = Ghost.agents
-    agents.select do |agent|
+    Ghost.agents.select do |agent|
       agent.username.downcase.include?(query)
+    end
+  end
+
+  def search_houses(query)
+    House.all.select do |house|
+      house.name.downcase.include?(query)
     end
   end
 
