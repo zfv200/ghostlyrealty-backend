e89a48fdd5b227578adf53fda7ca10047872c871
zfv200 <zach.vary@flatironschool.com>
2019-04-09 17:34:35 -0400

    mediums associated with haunts

--- db/migrate/20190408203852_add_medium_id_to_houses.rb
+++ db/migrate/20190408203852_add_medium_id_to_houses.rb
@@ -1,5 +1,5 @@
 class AddMediumIdToHouses < ActiveRecord::Migration[5.2]
   def change
-    add_reference :houses, :medium, foreign_key: {to_table: :ghosts} 
+    add_reference :houses, :medium, foreign_key: {to_table: :ghosts}
   end
 end
